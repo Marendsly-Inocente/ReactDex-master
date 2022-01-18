3bfac409536e5e6e98663a5df77ebfbe55723d68
Marendsly <509972@vistacollege.nl>
2022-01-18 09:53:25 +0100

    first commit

---
+++ build/precache-manifest.4fa4649fd344e4eadba373be4ea114ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "34369cf4e0ca56d3e0d7",
+    "url": "/ReactDex/static/css/main.39878a55.chunk.css"
+  },
+  {
+    "revision": "34369cf4e0ca56d3e0d7",
+    "url": "/ReactDex/static/js/main.34369cf4.chunk.js"
+  },
+  {
+    "revision": "7a5b2a0ba11745b9bd93",
+    "url": "/ReactDex/static/css/1.92396b95.chunk.css"
+  },
+  {
+    "revision": "7a5b2a0ba11745b9bd93",
+    "url": "/ReactDex/static/js/1.7a5b2a0b.chunk.js"
+  },
+  {
+    "revision": "bf8c3a743c6b72ed2dd3",
+    "url": "/ReactDex/static/js/runtime~main.bf8c3a74.js"
+  },
+  {
+    "revision": "91077f1d85e11627527b94cbb3dee9f2",
+    "url": "/ReactDex/index.html"
+  }
+];
